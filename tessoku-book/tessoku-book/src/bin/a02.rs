c1196693c0f68733440b6a918cb195fa6b781d2b
yu1hpa <yu.1hpa@gmail.com>
2023-07-22 23:28:09 +0900

    feat(A02): Add AC code

--- tessoku-book/tessoku-book/src/bin/a02.rs
+++ tessoku-book/tessoku-book/src/bin/a02.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize;n],
+    }
+
+    for ai in a {
+        if ai == x {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
