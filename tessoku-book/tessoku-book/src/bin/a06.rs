db1a72781ce3d5611f2fb1c653017dc3b1da65e7
yu1hpa <yu.1hpa@gmail.com>
2023-07-23 01:05:11 +0900

    feat(A06): Add AC code

--- tessoku-book/tessoku-book/src/bin/a06.rs
+++ tessoku-book/tessoku-book/src/bin/a06.rs
@@ -2,5 +2,25 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        q: usize,
+        a: [usize;n],
+        lr: [(usize, usize);q]
+    }
+
+    let mut sums: Vec<usize> = Vec::new();
+    for lri in lr {
+        let r = lri.0 - 1;
+        let l = lri.1 - 1;
+        let mut sum = 0;
+        for s in &a[r..=l] {
+            sum += s;
+        }
+        sums.push(sum);
+    }
+
+    for sum in sums {
+        println!("{}", sum);
+    }
 }
