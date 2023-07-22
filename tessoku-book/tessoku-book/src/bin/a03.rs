192fba45520a1147e6d651395a7d20d849deef6b
yu1hpa <yu.1hpa@gmail.com>
2023-07-22 23:35:14 +0900

    feat(A03): Add AC code

--- tessoku-book/tessoku-book/src/bin/a03.rs
+++ tessoku-book/tessoku-book/src/bin/a03.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        p: [usize;n],
+        q: [usize;n],
+    }
+    for pi in p {
+        for qi in q.clone() {
+            if (pi + qi) == k {
+                println!("Yes");
+                return;
+            }
+        }
+    }
+    println!("No");
 }
