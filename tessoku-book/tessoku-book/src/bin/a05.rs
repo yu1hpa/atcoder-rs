3cc529ad5e3a974a92bf66ea4942ecd1c7560da1
yu1hpa <yu.1hpa@gmail.com>
2023-07-23 00:37:24 +0900

    feat(A05): Add AC code

--- tessoku-book/tessoku-book/src/bin/a05.rs
+++ tessoku-book/tessoku-book/src/bin/a05.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize
+    }
+
+    let mut ans = 0;
+    for i in 1..=n {
+        for j in 1..=n {
+            if i + j < k && i + j + n >= k {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
