b2510ee7d8ae3dc8821ff6640b1c5ddd4bba01d1
yu1hpa <yu.1hpa@gmail.com>
2023-07-23 02:02:48 +0900

    feat(A07): Add AC code

--- tessoku-book/tessoku-book/src/bin/a07.rs
+++ tessoku-book/tessoku-book/src/bin/a07.rs
@@ -2,5 +2,19 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        d: usize,
+        n: usize,
+        lr: [(usize,usize);n],
+    }
+    let mut table = vec![0; d + 2];
+    for lri in lr {
+        table[lri.0] += 1;
+        table[lri.1 + 1] -= 1;
+    }
+
+    for i in 1..=d {
+        table[i] += table[i - 1];
+        println!("{}", table[i]);
+    }
 }
