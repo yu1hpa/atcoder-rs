66f903f30175c1d2696e5da0147bd3986ea7dc71
yu1hpa <yu.1hpa@gmail.com>
2023-07-24 13:32:44 +0900

    feat(A08): Add AC code

--- tessoku-book/tessoku-book/src/bin/a08.rs
+++ tessoku-book/tessoku-book/src/bin/a08.rs
@@ -2,5 +2,32 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        h: usize,
+        w: usize,
+        x: [[usize;w];h],
+        q: usize,
+        abcd: [(usize, usize, usize, usize);q],
+    }
+    let mut ex = vec![vec![0; w + 1]; h + 1];
+    // 横方向
+    for iy in 1..=h {
+        for ix in 1..=w {
+            ex[iy][ix] = x[iy - 1][ix - 1] + ex[iy][ix - 1];
+        }
+    }
+
+    // 縦方向
+    for iy in 1..=h {
+        for ix in 1..=w {
+            ex[iy][ix] += ex[iy - 1][ix];
+        }
+    }
+
+    for (a, b, c, d) in abcd {
+        println!(
+            "{}",
+            ex[c][d] + ex[a - 1][b - 1] - ex[a - 1][d] - ex[c][b - 1]
+        )
+    }
 }
