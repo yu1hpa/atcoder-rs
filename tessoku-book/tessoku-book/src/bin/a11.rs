0e79a56ce26dc6ac2a2236004a2e91e68db974b9
yu1hpa <yu.1hpa@gmail.com>
2023-07-29 15:12:50 +0900

    feat(A11): Add AC code

--- tessoku-book/tessoku-book/src/bin/a11.rs
+++ tessoku-book/tessoku-book/src/bin/a11.rs
@@ -1,6 +1,22 @@
 use proconio::{fastout, input};
 
+fn binary_search(a: Vec<usize>, start: usize, end: usize, v: usize) -> usize {
+    let point = (start + end) / 2;
+    if a[point] == v {
+        point
+    } else if a[point] < v {
+        binary_search(a, point, end, v)
+    } else {
+        binary_search(a, start, point, v)
+    }
+}
+
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        x: usize,
+        a: [usize;n],
+    }
+    println!("{}", binary_search(a, 0, n, x) + 1);
 }
