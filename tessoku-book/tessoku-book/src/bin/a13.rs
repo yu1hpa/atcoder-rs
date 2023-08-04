2f1d610959c0c197e149356263d247b54f7d1fcd
yu1hpa <yu.1hpa@gmail.com>
2023-08-04 17:15:24 +0900

    feat(A13): Add AC code

--- tessoku-book/tessoku-book/src/bin/a13.rs
+++ tessoku-book/tessoku-book/src/bin/a13.rs
@@ -2,5 +2,27 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        k: usize,
+        a: [usize;n],
+    }
+    let mut r = vec![0; n];
+    for i in 0..(n - 1) {
+        if i == 0 {
+            r[i] = 0;
+        } else {
+            r[i] = r[i - 1];
+        }
+
+        while r[i] < (n - 1) && (a[r[i] + 1] - a[i] <= k) {
+            r[i] += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..(n - 1) {
+        ans += r[i] - i;
+    }
+    println!("{}", ans);
 }
